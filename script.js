601cc5bc53f604d365ae5ce369c45c8faabea466
Amanda-gu <guoj826@newschool.edu>
2026-03-28 14:45:42 -0400

    added location

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
 
 // Below is for the grolocation api
-
 // live example: https://www.w3schools.com/html/tryit.asp?filename=tryhtml5_geolocation
 
-const x = document.getElementById("location");
+const locationName = document.getElementById("location");
+const findmeButton = document.getElementById('find-me');
 
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(success, error);
-  } else { 
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
-}
 
-function success(position) {
-  x.innerHTML = "Latitude: " + position.coords.latitude + 
-  "<br>Longitude: " + position.coords.longitude;
-}
+findmeButton.addEventListener('click', () => {   
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(success, error)
+        } else { 
+            locationName.innerHTML = "Geolocation is not supported by this browser."
+        }
+        }
 
-function error() {
-  alert("Sorry, no position available.");
-}
+    function success(position) {
+        locationName.innerHTML = "Latitude: " + position.coords.latitude + 
+        "<br>Longitude: " + position.coords.longitude
+        console.log("Your current position is:")
+        }
+
+    function error() {
+        locationName.innerHTML = "No location."
+        }
+//in the w3school example onclick function is in the html, so i moved it to my script.js, but then it was not working. 
+//i asked claude why that is. it was because i never told the click function to do the getLocation()
+//the three functions above were just setting it up but not actually running it...it can exist outside of the click funtion too
+//https://claude.ai/share/bb8f94ee-4a35-4cd7-b5f3-e308d2ad448f
+
+    getLocation()
+})
