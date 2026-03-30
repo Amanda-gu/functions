08d4f79694ecdd67382a6b9da89fbe9a9744989f
Amanda-gu <guoj826@newschool.edu>
2026-03-30 13:28:41 -0400

    wrting down logic

---
+++ assets/scripts.js
@@ -0,0 +1,14 @@
+output:
+name,
+base + mixers + garnish
+
+if occasion is brunch
+base: abv below 6
+
+if weather is 20, 23, 24 & temp high (clear sky)
+base taste:{clear, crisp} 
+mixers taste: {clear, crisp} 
+garnish taste: {earthy, aromic} 
+
+
+
