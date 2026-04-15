ffb4ae5935b067f39785155db6ca66d17e20270f
Amanda-gu <guoj826@newschool.edu>
2026-04-15 18:23:41 -0400

    update spacing for share card

--- assets/scripts.js
+++ assets/scripts.js
@@ -338,7 +338,7 @@ let showCard = (data) => {
 		shareBtn.addEventListener("click", async () => {
 				//turn html elemetns into image: https://www.youtube.com/watch?v=rIubDKHy0js
 				//what i learned: turn it into canvas so i can get the image from the canvas
-				html2canvas(ingredientList).then(canvas => {
+				html2canvas(dialogCard).then(canvas => {
 					//give it a size
 					//asked chat gpt in this thread:  https://chatgpt.com/share/69d6828e-7360-8327-83e1-29deb4cbe844
 					//i cant change the size of blob because its raw data.
